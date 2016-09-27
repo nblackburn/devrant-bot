bd994719fee35013fa0e4c1a0706c353e72b98cb
Nathaniel Blackburn <support@nblackburn.uk>
2016-09-27 19:22:33 +0100

    Cleanup

--- source/events.js
+++ source/events.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const MENTION        = 'mention';
-const MESSAGE        = 'message';
+const MENTION = 'mention';
+const MESSAGE = 'message';
 const DIRECT_MESSAGE = 'direct_message';
 const DIRECT_MENTION = 'direct_mention';
 
 exports.MENTION = MENTION;
 exports.MESSAGE = MESSAGE;
-exports.DIRECT_MENTION = DIRECT_MENTION;
 exports.DIRECT_MESSAGE = DIRECT_MESSAGE;
+exports.DIRECT_MENTION = DIRECT_MENTION;
